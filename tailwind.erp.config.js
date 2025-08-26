b0818d0f1c3210afda3196db1614daa28a19d7cf
DHCross <45954119+DHCross@users.noreply.github.com>
2025-08-25 21:36:06 -0500

    small fixes

--- tailwind.erp.config.js
+++
@@ -1,7 +0,0 @@
-// Tailwind config dedicated to the Eldritch RPG offshoot page to keep
-// its utility set isolated from the main Woven app bundle.
-module.exports = {
-  content: ["./erp_pcs.html"],
-  theme: { extend: {} },
-  plugins: [],
-};
